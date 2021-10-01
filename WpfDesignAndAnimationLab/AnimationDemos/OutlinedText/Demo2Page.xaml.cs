b8fd9a91b15f5cfa6366862749a73b4f6da16ef1
Dino Chan <dino.chan@live.com>
2021-10-01 23:45:12 +0800

    add demo page

---
+++ WpfDesignAndAnimationLab/AnimationDemos/OutlinedText/Demo2Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDesignAndAnimationLab.AnimationDemos.OutlinedText
+{
+    /// <summary>
+    /// Demo2Page.xaml 的交互逻辑
+    /// </summary>
+    public partial class Demo2Page : UserControl
+    {
+        public Demo2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
