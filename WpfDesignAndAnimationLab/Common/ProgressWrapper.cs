4c36ccec56157cd6b421de3e682160e09c37962f
Dino Chan <dino.chan@live.com>
2021-10-02 21:53:34 +0800

    add common codes

---
+++ WpfDesignAndAnimationLab/Common/ProgressWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfDesignAndAnimationLab.Common
+{
+    public class ProgressWrapper : DependencyObject
+    {
+
+        /// <summary>
+        /// 获取或设置Progress的值
+        /// </summary>  
+        public double Progress
+        {
+            get { return (double)GetValue(ProgressProperty); }
+            set { SetValue(ProgressProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 Progress 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty ProgressProperty =
+            DependencyProperty.Register("Progress", typeof(double), typeof(ProgressWrapper), new PropertyMetadata(0d));
+    }
+
+}
---
+++ WpfDesignAndAnimationLab/Converters/ProgressToHeightConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfDesignAndAnimationLab.Converters
+{
+    public class ProgressToHeightConverter : DependencyObject, IValueConverter
+    {
+        /// <summary>
+        /// 获取或设置TargetContentControl的值
+        /// </summary>  
+        public ContentControl TargetContentControl
+        {
+            get { return (ContentControl)GetValue(TargetContentControlProperty); }
+            set { SetValue(TargetContentControlProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 TargetContentControl 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty TargetContentControlProperty =
+            DependencyProperty.Register("TargetContentControl", typeof(ContentControl), typeof(ProgressToHeightConverter), new PropertyMetadata(null));
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double == false)
+                return 0d;
+
+            var progress = (double)value;
+
+            if (TargetContentControl == null)
+                return 0d;
+
+            var element = TargetContentControl.Content as FrameworkElement;
+            if (element == null)
+                return 0d;
+
+            return element.Height * progress / 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+  
+}
