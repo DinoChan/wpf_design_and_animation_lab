6280542ff0b16efaa2259a7b993034ac0262c026
DinoChan <dino.chan@live.com>
2021-10-20 16:46:49 +0800

    add some code

--- WpfDesignAndAnimationLab/AnimationDemos/OutlinedText/Demo1Page.xaml.cs
+++ WpfDesignAndAnimationLab/AnimationDemos/OutlinedText/Demo1Page.xaml.cs
@@ -26,6 +26,37 @@ namespace WpfDesignAndAnimationLab.AnimationDemos.OutlinedText
             var backColor = System.Drawing.ColorTranslator.FromHtml("#037be2");
             var forColor = System.Drawing.ColorTranslator.FromHtml("#ffffff");
             img.Source = FancyText.BitmapImageFromText("测试字体，微软雅黑", new System.Drawing.Font("Microsoft YaHei", 60, System.Drawing.FontStyle.Bold), forColor, backColor, 6);
+            Loaded += Demo1Page_Loaded;
         }
+
+        private void Demo1Page_Loaded(object sender, RoutedEventArgs e)
+        {
+           var l= GetLength(TextPath.RenderedGeometry);
+        }
+
+        public  double GetLength( Geometry geo)
+        {
+            PathGeometry path = geo.GetFlattenedPathGeometry();
+
+            double length = 0.0;
+
+            foreach (PathFigure pf in path.Figures)
+            {
+                Point start = pf.StartPoint;
+
+                foreach (PolyLineSegment seg in pf.Segments)
+                {
+                    foreach (Point point in seg.Points)
+                    {
+                        length += (start - point).Length;
+                        start = point;
+                    }
+                }
+            }
+
+            return length;
+        }
+
+      
     }
 }
