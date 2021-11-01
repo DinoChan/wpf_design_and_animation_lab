6672da9fa0f1fe5da7dbc1a6a020c7e0a5999b45
DinoChan <dino.chan@live.com>
2021-11-01 16:46:14 +0800

    add radial prgress demo

---
+++ WpfDesignAndAnimationLab/Behaviors/EllipseProgressBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Xaml.Behaviors;
+
+namespace WpfDesignAndAnimationLab.Behaviors
+{
+    public class EllipseProgressBehavior : Behavior<Ellipse>
+    {
+        /// <summary>
+        /// 获取或设置Progress的值
+        /// </summary>  
+        public double Progress
+        {
+            get { return (double)GetValue(ProgressProperty); }
+            set { SetValue(ProgressProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 Progress 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty ProgressProperty =
+            DependencyProperty.Register("Progress", typeof(double), typeof(EllipseProgressBehavior), new PropertyMetadata(0d, OnProgressChanged));
+
+        private static void OnProgressChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var target = obj as EllipseProgressBehavior;
+            double oldValue = (double)args.OldValue;
+            double newValue = (double)args.NewValue;
+            if (oldValue != newValue)
+                target.OnProgressChanged(oldValue, newValue);
+        }
+
+
+        protected virtual void OnProgressChanged(double oldValue, double newValue)
+        {
+            UpdateStrokeDashArray();
+        }
+
+        protected virtual double GetTotalLength()
+        {
+            if (AssociatedObject == null)
+                return 0;
+
+            return (AssociatedObject.ActualHeight - AssociatedObject.StrokeThickness) * Math.PI;
+        }
+
+
+        private void UpdateStrokeDashArray()
+        {
+            if (AssociatedObject == null || AssociatedObject.StrokeThickness == 0)
+                return;
+
+            //if (target.ActualHeight == 0 || target.ActualWidth == 0)
+            //    return;
+
+            var totalLength = GetTotalLength();
+            totalLength = totalLength / AssociatedObject.StrokeThickness;
+            var thirdSection = Progress * totalLength / 100;
+            var secondSection = (totalLength - thirdSection) / 2;
+            var result = new DoubleCollection { 0, secondSection, thirdSection, double.MaxValue };
+            AssociatedObject.StrokeDashArray = result;
+        }
+
+    }
+}
