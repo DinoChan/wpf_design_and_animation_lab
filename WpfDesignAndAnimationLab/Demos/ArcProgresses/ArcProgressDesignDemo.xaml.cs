4844ca5bf23fe5de871deb981acd70c86ce84fb2
DinoChan <dino.chan@live.com>
2021-11-03 16:56:18 +0800

    添加备注

--- WpfDesignAndAnimationLab/Demos/ArcProgresses/ArcProgressDesignDemo.xaml.cs
+++ WpfDesignAndAnimationLab/Demos/ArcProgresses/ArcProgressDesignDemo.xaml.cs
@@ -17,6 +17,7 @@ namespace WpfDesignAndAnimationLab.Demos.ArcProgresses
 {
     /// <summary>
     /// http://preview.binlive.cn/arc-progress
+    /// https://codepen.io/nathgreen/pen/gdwZaE
     /// </summary>
     public partial class ArcProgressDesignDemo 
     {
