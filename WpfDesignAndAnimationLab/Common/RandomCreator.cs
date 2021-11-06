ac7c3fb1106f4bdd25f709a7e376d697cbdd65f8
Dino Chan <dino.chan@live.com>
2021-11-06 10:14:32 +0800

    add RandomDoubleCreator

---
+++ WpfDesignAndAnimationLab/Common/RandomCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace WpfDesignAndAnimationLab.Common
+{
+    public abstract class RandomCreator<T> : DependencyObject
+    {
+        public T Max { get; set; }
+
+        public abstract T Next { get; }
+    }
+}
---
+++ WpfDesignAndAnimationLab/Common/RandomDoubleCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDesignAndAnimationLab.Common
+{
+    public class RandomDoubleCreator : RandomCreator<double>
+    {
+        private Random _random = new Random();
+
+        public override double Next => _random.NextDouble() * _random.Next(Convert.ToInt32(Math.Round(Max)));
+    }
+}
