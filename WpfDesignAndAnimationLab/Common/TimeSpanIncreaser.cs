7c5da652d9126a9695d41043d9665518ed8b3a16
DinoChan <dino.chan@live.com>
2021-11-07 15:24:30 +0800

    format xaml

--- WpfDesignAndAnimationLab/Common/TimeSpanIncreaser.cs
+++ WpfDesignAndAnimationLab/Common/TimeSpanIncreaser.cs
@@ -1,31 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfDesignAndAnimationLab.Common
 {
     public class TimeSpanIncreaser : Increaser<TimeSpan>
     {
+        // Using a DependencyProperty as the backing store for Start.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartProperty =
+            DependencyProperty.Register("Start", typeof(TimeSpan), typeof(TimeSpanIncreaser), new PropertyMetadata(default(TimeSpan)));
 
         private TimeSpan _current;
 
         public override TimeSpan Next => Start + (_current += Step);
-
-
 
         public override TimeSpan Start
         {
             get { return (TimeSpan)GetValue(StartProperty); }
             set { SetValue(StartProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for Start.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty StartProperty =
-            DependencyProperty.Register("Start", typeof(TimeSpan), typeof(TimeSpanIncreaser), new PropertyMetadata(default(TimeSpan)));
-
-
     }
 }
