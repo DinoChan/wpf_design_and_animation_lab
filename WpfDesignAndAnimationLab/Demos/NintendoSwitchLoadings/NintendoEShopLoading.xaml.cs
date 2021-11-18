30b1fe6a88f21349caab1cded83eea6570d424da
DinoChan <dino.chan@live.com>
2021-11-18 20:31:38 +0800

    add a loading page

---
+++ WpfDesignAndAnimationLab/Demos/NintendoSwitchLoadings/NintendoEShopLoading.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDesignAndAnimationLab.Demos.NintendoSwitchLoadings
+{
+    /// <summary>
+    /// NintendoEShopLoading.xaml 的交互逻辑
+    /// https://codepen.io/jh3y/pen/XYxJBm
+    /// </summary>
+    public partial class NintendoEShopLoading 
+    {
+        public NintendoEShopLoading()
+        {
+            InitializeComponent();
+        }
+    }
+}
